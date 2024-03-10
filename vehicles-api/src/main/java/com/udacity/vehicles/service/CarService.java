5afba5926ebe18fd07dfd6adbb46347ea740a96e
Steven Nguyen <nguyenbaotrung.hcm@gmail.com>
2024-03-10 16:11:11 +0700

    Clean up

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -44,33 +44,12 @@ public class CarService {
      * @return the requested car's information, including location and price
      */
     public Car findById(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         * If it does not exist, throw a CarNotFoundException
-         * Remove the below code as part of your implementation.
-         */
         Car car = repository.findById(id).orElseThrow(CarNotFoundException::new);
         Location location = mapsClient.getAddress(car.getLocation());
         String price = priceClient.getPrice(id);
         car.setLocation(location);
         car.setPrice(price);
-        /**
-         * TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
-         * to get the price based on the `id` input'
-         * TODO: Set the price of the car
-         * Note: The car class file uses @transient, meaning you will need to call
-         * the pricing service each time to get the price.
-         */
-
-        /**
-         * TODO: Use the Maps Web client you create in `VehiclesApiApplication`
-         * to get the address for the vehicle. You should access the location
-         * from the car object and feed it to the Maps service.
-         * TODO: Set the location of the vehicle, including the address information
-         * Note: The Location class file also uses @transient for the address,
-         * meaning the Maps service needs to be called each time for the address.
-         */
-
+    
         return car;
     }
 
@@ -99,10 +78,6 @@ public class CarService {
      * @param id the ID number of the car to delete
      */
     public void delete(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         * If it does not exist, throw a CarNotFoundException
-         */
         if (repository.existsById(id)) {
             repository.deleteById(id);
             return;
